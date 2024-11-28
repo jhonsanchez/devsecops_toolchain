5b186b56d310e41d15403e4a41341649d8284d08
Jhon Sanchez <jhon.sanchez@zebra.com>
2024-11-28 13:12:32 -0500

    jenkins only

--- sonar.tf
+++
@@ -1,8 +0,0 @@
-resource "helm_release" "sonarqube" {
-  name             = "sonarqube"
-  namespace        = "sonarqube"
-  create_namespace = true
-
-  repository = "https://SonarSource.github.io/helm-chart-sonarqube"
-  chart      = "sonarqube"
-}
